7278182b677ed326be70e6681f32b8f53643245a
Christina <34919576+Maraakis@users.noreply.github.com>
2023-12-19 14:49:57 +0100

    Add files via upload

---
+++ Toggle Switch Styling/ToggleSwitchColorChange.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Christina.UI
+{
+    public class ToggleSwitchColorChange : ToggleSwitch
+    {
+        [Header("Elements to Recolor")]
+        [SerializeField] private Image backgroundImage;
+        [SerializeField] private Image handleImage;
+        
+        [Space]
+        [SerializeField] private bool recolorBackground;
+        [SerializeField] private bool recolorHandle;
+        
+        [Header("Colors")]
+        [SerializeField] private Color backgroundColorOff = Color.white;
+        [SerializeField] private Color backgroundColorOn = Color.white;
+        [Space]
+        [SerializeField] private Color handleColorOff = Color.white;
+        [SerializeField] private Color handleColorOn = Color.white;
+        
+        private bool _isBackgroundImageNotNull;
+        private bool _isHandleImageNotNull;
+        
+        private new void OnValidate()
+        {
+            base.OnValidate();
+            
+            CheckForNull();
+            ChangeColors();
+        }
+
+        private void OnEnable()
+        {
+            transitionEffect += ChangeColors;
+        }
+        
+        private void OnDisable()
+        {
+            transitionEffect -= ChangeColors;
+        }
+
+        protected override void Awake() 
+        {
+            base.Awake();
+            
+            CheckForNull();
+            ChangeColors();
+        }
+
+        private void CheckForNull()
+        {
+            _isBackgroundImageNotNull = backgroundImage != null;
+            _isHandleImageNotNull = handleImage != null;
+        }
+
+
+        private void ChangeColors()
+        {
+            if (recolorBackground && _isBackgroundImageNotNull)
+                backgroundImage.color = Color.Lerp(backgroundColorOff, backgroundColorOn, sliderValue); 
+            
+            if (recolorHandle && _isHandleImageNotNull)
+                handleImage.color = Color.Lerp(handleColorOff, handleColorOn, sliderValue); 
+        }
+    }
+}
+
+
+
+
---
+++ Toggle Switch Styling/ToggleSwitchMaterialChange.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Christina.UI
+{
+    public class ToggleSwitchMaterialChange : ToggleSwitch
+    {
+        [Header("Background")]
+        [SerializeField] private Image backgroundImage;
+        [SerializeField] private Material backgroundMaterial;
+        private Material _localCopyOfBackgroundMaterial;
+        
+        [Space]
+        [SerializeField] private Image handleImage;
+        [SerializeField] private Material handleMaterial;
+        private Material _localCopyOfHandleMaterial;
+        
+        private bool _isBackgroundImageNotNull;
+        private bool _isHandleImageNotNull;
+        
+        private bool _isBackgroundMaterialNotNull;
+        private bool _isHandleMaterialNotNull;
+
+        private new void OnValidate()
+        {
+            base.OnValidate();
+            
+            SetupBackgroundMaterial();
+            SetupHandleMaterial();
+            
+            TransitionImages();
+        }
+        
+        protected override void Awake() 
+        {
+            base.Awake();
+            
+            _isHandleMaterialNotNull = handleImage.material != null;
+            _isBackgroundMaterialNotNull = backgroundImage.material != null;
+            _isHandleImageNotNull = handleImage != null;
+            _isBackgroundImageNotNull = backgroundImage != null;
+            
+            SetupBackgroundMaterial();
+            SetupHandleMaterial();
+            TransitionImages();
+        }
+        
+        private void SetupHandleMaterial()
+        {
+            _localCopyOfHandleMaterial = new Material(handleMaterial);
+            
+            if (_isHandleImageNotNull)
+                handleImage.material = _localCopyOfHandleMaterial;
+        }
+
+        private void SetupBackgroundMaterial()
+        {
+            _localCopyOfBackgroundMaterial = new Material(backgroundMaterial);
+            
+            if (_isBackgroundImageNotNull)
+                backgroundImage.material = _localCopyOfBackgroundMaterial;
+        }
+
+        
+        private void OnEnable()
+        {
+            transitionEffect += TransitionImages;
+        }
+        
+        private void OnDisable()
+        {
+            transitionEffect -= TransitionImages;
+        }
+
+        private void TransitionImages()
+        {
+            if (_isBackgroundImageNotNull && _isBackgroundMaterialNotNull)
+                backgroundImage.material.SetFloat("_MixValue", sliderValue); 
+
+            if (_isHandleImageNotNull && _isHandleMaterialNotNull)
+                handleImage.material.SetFloat("_MixValue", sliderValue);
+        }
+    }
+}
+
+
+
+
+
